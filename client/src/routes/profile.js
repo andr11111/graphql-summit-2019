81d3bd8e526e404094f4d42da5b152e2fc669be0
Andrei Anisimov <1471231+andrei-anisimov@users.noreply.github.com>
2019-10-29 22:37:53 -0700

    clean

--- client/src/routes/profile.js
+++ client/src/routes/profile.js
@@ -3,174 +3,123 @@ import gql from 'graphql-tag';
 import { useQuery, useMutation } from 'react-apollo';
 import useForm from 'react-hook-form'
 
-const PROFILE_QUERY = gql`
-  query ProfileQuery {
-    user {
-      id
-      email
-      lastName
-      firstName
-      salesforceAccount {
-        Name
-        Policies__r {
-          count
-          items {
-            id
-            Name
-            StartDate__c
-            EndDate__c
-            Active__c
-          }
-        }
-      }
-    }
-  }
-`;
+// const PROFILE_QUERY = gql``;
 
-const CLAIMS_QUERY = gql`
-  query ClaimsQuery($policyId: ID!) {
-    salesforce {
-      policy__c(id: $policyId) {
-        claims {
-          items {
-            id
-            createdAt
-            description
-            documents {
-              items {
-                downloadUrl
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
+// const CLAIMS_QUERY = gql``;
 
-const CREATE_CLAIM = gql`
-  mutation CreateClaim($description: String!, $policyId: ID!) {
-    claimCreate(data: {
-      description: $description
-      policy: {
-        connect: {
-          id: $policyId
-        }
-      }
-    }) {
-      id
-      description
-      documents {
-        items {
-          filename
-          downloadUrl
-        }
-      }
-    }
-  }
-`;
+// const CREATE_CLAIM = gql``;
 
 
-let AccountInfo = () => {
-  const { loading, data } = useQuery(PROFILE_QUERY);
+// let AccountInfo = () => {
+//   const { loading, data } = useQuery(PROFILE_QUERY);
 
-  if (loading) return <span>Loading...</span>;
+//   if (loading) return <span>Loading...</span>;
 
-  return (
-    <ul>
-      <li>
-        Name: {data.user.salesforceAccount.Name}
-      </li>
-      <li>
-        Email: {data.user.email}
-      </li>
-    </ul>
-  )
-}
+//   return (
+//     <ul>
+//       <li>
+//         Name: {data.user.salesforceAccount.Name}
+//       </li>
+//       <li>
+//         Email: {data.user.email}
+//       </li>
+//     </ul>
+//   )
+// }
 
-let Policies = ({ selectedPolicyId, onSelectPolicy }) => {
-  const { loading, data } = useQuery(PROFILE_QUERY);
+// let Policies = ({ selectedPolicyId, onSelectPolicy }) => {
+//   const { loading, data } = useQuery(PROFILE_QUERY);
 
-  if (loading) return <span>Loading...</span>;
+//   if (loading) return <span>Loading...</span>;
 
-  let cssClass = (selected) => selected ? 'clickable selected' : 'clickable';
+//   let cssClass = (selected) => selected ? 'clickable selected' : 'clickable';
 
-  const policyItems = data.user.salesforceAccount.Policies__r.items.map((policy) =>
-    <li className={cssClass(selectedPolicyId === policy.id)} key={policy.id} onClick={() => onSelectPolicy(policy.id)}>
-      {policy.Name}, from: {policy.StartDate__c}, to: {policy.EndDate__c}
-    </li>
-  )
+//   const policyItems = data.user.salesforceAccount.Policies__r.items.map((policy) =>
+//     <li className={cssClass(selectedPolicyId === policy.id)} key={policy.id} onClick={() => onSelectPolicy(policy.id)}>
+//       {policy.Name}, from: {policy.StartDate__c}, to: {policy.EndDate__c}
+//     </li>
+//   )
 
-  return (
-    <ul>{ policyItems }</ul>
-  )
-}
+//   return (
+//     <ul>{ policyItems }</ul>
+//   )
+// }
 
-let Claims = ({ policyId }) => {
-  const { loading, data } = useQuery(CLAIMS_QUERY, { variables: { policyId }});
-  if (loading) return <span>Loading...</span>;
+// let Claims = ({ policyId }) => {
+//   const { loading, data } = useQuery(CLAIMS_QUERY, { variables: { policyId }});
+//   if (loading) return <span>Loading...</span>;
 
-  const claimItems = data.salesforce.policy__c.claims.items.map(claim =>
-    <li key={claim.id}>{claim.description}, created on {new Date(claim.createdAt).toLocaleDateString()}</li>
-  )
+//   const claimItems = data.salesforce.policy__c.claims.items.map(claim =>
+//     <li key={claim.id}>{claim.description}, created on {new Date(claim.createdAt).toLocaleDateString()}</li>
+//   )
 
-  return (
-    <ul>{ claimItems }</ul>
-  )
-}
+//   return (
+//     <ul>{ claimItems }</ul>
+//   )
+// }
 
-let NewClaimForm = ({ policyId, onSubmit }) => {
-  const { register, handleSubmit, watch, errors } = useForm()
+// let NewClaimForm = ({ policyId, onSubmit }) => {
+//   const { register, handleSubmit, watch, errors } = useForm()
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-    {/* register your input into the hook by invoking the "register" function */}
-      <input name="description" defaultValue="" ref={register({ required: true })} />
-      <input type="submit" />
-    </form>
-  )
-}
+//   return (
+//     <form onSubmit={handleSubmit(onSubmit)}>
+//     {/* register your input into the hook by invoking the "register" function */}
+//       <input name="description" defaultValue="" ref={register({ required: true })} />
+//       <input type="submit" />
+//     </form>
+//   )
+// }
+
+// let Profile = () => {
+//   const [policyId, setPolicyId] = useState(null);
+
+//   const [showForm, setShowForm] = useState(false);
+//   const [createClaim, { data }] = useMutation(CREATE_CLAIM);
+
+//   const onSubmit = async data => {
+//     await createClaim({
+//       variables: {
+//         description: data.description,
+//         policyId
+//       },
+//       refetchQueries: ['ClaimsQuery']
+//     })
+//     setShowForm(false)
+//   }
+
+//   const selectPolicy = (policyId) => {
+//     setPolicyId(policyId)
+//     setShowForm(false)
+//   }
+
+//   return (
+//       <div>
+//         <h1>Account Info</h1>
+//         <AccountInfo />
+//         <h1>Policies</h1>
+//         <Policies selectedPolicyId={policyId} onSelectPolicy={selectPolicy} />
+//         { policyId &&
+//           <>
+//             <h1>Claims (<span className='clickable' onClick={() => setShowForm(true)}>+</span>)</h1>
+//             <Claims policyId={policyId} />
+//           </>
+//         }
+//         { showForm &&
+//           <>
+//             <h1>Submit Claim</h1>
+//             <NewClaimForm policyId={policyId} onSubmit={onSubmit} />
+//           </>
+//         }
+//       </div>
+//   )
+// };
 
 let Profile = () => {
-  const [policyId, setPolicyId] = useState(null);
-
-  const [showForm, setShowForm] = useState(false);
-  const [createClaim, { data }] = useMutation(CREATE_CLAIM);
-
-  const onSubmit = async data => {
-    await createClaim({
-      variables: {
-        description: data.description,
-        policyId
-      },
-      refetchQueries: ['ClaimsQuery']
-    })
-    setShowForm(false)
-  }
-
-  const selectPolicy = (policyId) => {
-    setPolicyId(policyId)
-    setShowForm(false)
-  }
-
   return (
       <div>
         <h1>Account Info</h1>
-        <AccountInfo />
         <h1>Policies</h1>
-        <Policies selectedPolicyId={policyId} onSelectPolicy={selectPolicy} />
-        { policyId &&
-          <>
-            <h1>Claims (<span className='clickable' onClick={() => setShowForm(true)}>+</span>)</h1>
-            <Claims policyId={policyId} />
-          </>
-        }
-        { showForm &&
-          <>
-            <h1>Submit Claim</h1>
-            <NewClaimForm policyId={policyId} onSubmit={onSubmit} />
-          </>
-        }
+        <h1>Claims</h1>
       </div>
   )
 };
--- server/src/userBeforeCreate.ts
+++ server/src/userBeforeCreate.ts
@@ -1,26 +1,5 @@
 import gql from 'graphql-tag';
 
-/**
- * This file was generated using 8base CLI.
- *
- * To learn more about writing custom trigger functions, visit
- * the 8base documentation at:
- *
- * https://docs.8base.com/8base-console/custom-functions/triggers
- *
- * To update this functions invocation settings, update its configuration block
- * in the projects 8base.yml file:
- *  functions:
- *    trigger:
- *      ...
- *
- * Data that is sent to the function can be accessed on the event argument at:
- *  event.data[KEY_NAME]
- *
- * To invoke this function locally, run:
- *  8base invoke-local trigger -p src/triggers/trigger/mocks/request.json
- */
-
 type TriggerResult = {
   data: {
     result: string,
@@ -30,7 +9,7 @@ type TriggerResult = {
 export default async (event: any, ctx: any) : Promise<TriggerResult> => {
   const { email } = event.data
 
-  console.log("TRIGGER TEST!!!!")
+  console.log("TEST: ", email)
 
   return {
     data: {
